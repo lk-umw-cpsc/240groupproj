46de86503b316c11cfbb61d7e9dbd7fc0284ccd3
Lauren Knight <lknight2@mail.umw.edu>
2022-03-26 11:03:13 -0400

    Cleanup

--- code/BackgroundDaemon.java
+++ code/BackgroundDaemon.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
