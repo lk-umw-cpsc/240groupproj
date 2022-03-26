0f4b0c76cd49fc555ea7fb6a766a42849de4d953
this-is-forever <95399490+lk-umw-cpsc@users.noreply.github.com>
2022-03-26 09:05:40 -0400

    Tweak methods

--- code/schedule/ScheduleIO.java
+++ code/schedule/ScheduleIO.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Scanner;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,13 +11,13 @@ import java.io.PrintWriter;
 public class ScheduleIO 
 {
 
-        public static void loadSchedules(List<ScheduledEvent> events, List<ScheduledReminder> reminders) throws FileNotFoundException
+        public static void loadSchedule(List<ScheduledReminder> reminders, List<ScheduledEvent> events)
         {
                 loadEvents(events);
                 loadReminders(reminders);
         }
 
-        public static void saveSchedules(List<ScheduledEvent> events, List<ScheduledReminder> reminders) throws IOException
+        public static void saveSchedule(List<ScheduledReminder> reminders, List<ScheduledEvent> events)
         {
                 saveEvents(events);
                 saveReminders(reminders);
@@ -26,82 +25,89 @@ public class ScheduleIO
         }
 
         
-        public static void loadEvents(List<ScheduledEvent> events) throws FileNotFoundException
+        public static void loadEvents(List<ScheduledEvent> events)
         {
 
-                FileInputStream fileInName = new FileInputStream("240GROUPPROJ/../data/Events.txt");
-                Scanner fileIn = new Scanner(fileInName);
+                try (FileInputStream fileInName = new FileInputStream("240GROUPPROJ/../data/Events.txt")) {
+                        Scanner fileIn = new Scanner(fileInName);
 
-                //this deletes the first line which contains Description###StartDateTStartTime###Duration
-                String newLine = fileIn.nextLine();
-                while (fileIn.hasNextLine())
-                {
-                        newLine = fileIn.nextLine();
-                        String[] parser = newLine.split("###");
-                        String[] splitTimeParser = parser[1].split("T");
-                        ScheduledEvent temp = new ScheduledEvent(parser[0], splitTimeParser[0], splitTimeParser[1], Integer.parseInt(parser[2]));
-                        events.add(temp);
+                        //this deletes the first line which contains Description###StartDateTStartTime###Duration
+                        String newLine = fileIn.nextLine();
+                        while (fileIn.hasNextLine())
+                        {
+                                newLine = fileIn.nextLine();
+                                String[] parser = newLine.split("###");
+                                String[] splitTimeParser = parser[1].split("T");
+                                ScheduledEvent temp = new ScheduledEvent(parser[0], splitTimeParser[0], splitTimeParser[1], Integer.parseInt(parser[2]));
+                                events.add(temp);
+                        }
+
+                } catch (IOException e) {
+                        e.printStackTrace();
                 }
-
-                fileIn.close();
 
 
         }
 
-        public static void loadReminders(List<ScheduledReminder> reminders) throws FileNotFoundException
+        public static void loadReminders(List<ScheduledReminder> reminders)
         {
 
-                FileInputStream fileInName = new FileInputStream("240GROUPPROJ/../data/Reminders.txt");
-                Scanner fileIn = new Scanner(fileInName);
+                try (FileInputStream fileInName = new FileInputStream("240GROUPPROJ/../data/Reminders.txt")) {
+                        Scanner fileIn = new Scanner(fileInName);
 
-                //this deletes the first line which contains Name###Description###Date###Reps
-                String newLine = fileIn.nextLine();
-                while (fileIn.hasNextLine())
-                {
-                        newLine = fileIn.nextLine();
-                        String[] parser = newLine.split("###");
-                        String[] splitTimeParser = parser[2].split("T");
-                        ScheduledReminder temp = new ScheduledReminder(parser[0], parser[1], splitTimeParser[0], splitTimeParser[1], Integer.parseInt(parser[3]));
-                        reminders.add(temp);
+                        //this deletes the first line which contains Name###Description###Date###Reps
+                        String newLine = fileIn.nextLine();
+                        while (fileIn.hasNextLine())
+                        {
+                                newLine = fileIn.nextLine();
+                                String[] parser = newLine.split("###");
+                                String[] splitTimeParser = parser[2].split("T");
+                                ScheduledReminder temp = new ScheduledReminder(parser[0], parser[1], splitTimeParser[0], splitTimeParser[1], Integer.parseInt(parser[3]));
+                                reminders.add(temp);
+                        }
+
+                } catch (IOException e) {
+                        e.printStackTrace();
                 }
-
-                fileIn.close();
 
         }
 
         
-        public static void saveEvents(List<ScheduledEvent> events) throws IOException
+        public static void saveEvents(List<ScheduledEvent> events)
         {
                 
-                FileOutputStream fileOutName = new FileOutputStream("240GROUPPROJ/../data/Events.txt");
-                PrintWriter fileOut = new PrintWriter(fileOutName);
+                try (FileOutputStream fileOutName = new FileOutputStream("240GROUPPROJ/../data/Events.txt")) {
+                        PrintWriter fileOut = new PrintWriter(fileOutName);
 
-                fileOut.println("Description###StartDateTStartTime###Duration");
-                for (ScheduledEvent event : events)
-                {
-                        fileOut.println(event.getDescription() + "###" + event.getStartTime() + "###" + event.getDuration());
+                        fileOut.println("Description###StartDateTStartTime###Duration");
+                        for (ScheduledEvent event : events)
+                        {
+                                fileOut.println(event.getDescription() + "###" + event.getStartTime() + "###" + event.getDuration());
+                        }
+
+                        fileOut.flush();
+                } catch (IOException e) {
+                        e.printStackTrace();
                 }
-
-                fileOut.flush();
-                fileOutName.close();
-
 
         }
 
-        public static void saveReminders(List<ScheduledReminder> reminders) throws IOException
+        public static void saveReminders(List<ScheduledReminder> reminders)
         {
 
-                FileOutputStream fileOutName = new FileOutputStream("240GROUPPROJ/../data/Reminders.txt");
-                PrintWriter fileOut = new PrintWriter(fileOutName);
+                try (FileOutputStream fileOutName = new FileOutputStream("240GROUPPROJ/../data/Reminders.txt")) {
+                        PrintWriter fileOut = new PrintWriter(fileOutName);
 
-                fileOut.println("Name###Description###Date###Reps");
-                for (ScheduledReminder reminder : reminders)
-                {
-                        fileOut.println(reminder.getName() + "###" + reminder.getDescription() + "###" + reminder.getWhenDue() + "###" + reminder.getDaysBetweenRepetitions());
+                        fileOut.println("Name###Description###Date###Reps");
+                        for (ScheduledReminder reminder : reminders)
+                        {
+                                fileOut.println(reminder.getName() + "###" + reminder.getDescription() + "###" + reminder.getWhenDue() + "###" + reminder.getDaysBetweenRepetitions());
+                        }
+
+                        fileOut.flush();
+                } catch(IOException e) {
+                        e.printStackTrace();
                 }
-
-                fileOut.flush();
-                fileOutName.close();
 
         }
 
