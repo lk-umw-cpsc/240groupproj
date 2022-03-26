2ff7e7cc4d445920b259a3ccca51fc6c901b0a29
this-is-forever <95399490+lk-umw-cpsc@users.noreply.github.com>
2022-03-26 10:11:48 -0400

    remove extra semicolon

--- code/ui/systemtray/SystemTrayManager.java
+++ code/ui/systemtray/SystemTrayManager.java
@@ -237,7 +237,7 @@ public class SystemTrayManager {
     }
 
     private void manageReminders(ActionEvent e) {
-        reminderManagerFrame.appear();;
+        reminderManagerFrame.appear();
     }
 
     private void exit(ActionEvent e) 
