59a71546ebc6a5855ed97f3150cc58d04ab84d7a
Lauren Knight <lknight2@mail.umw.edu>
2022-04-10 12:31:17 -0400

    implemented interface to allow other calendar notes

--- code/schedule/ScheduledEvent.java
+++ code/schedule/ScheduledEvent.java
@@ -1,7 +1,17 @@
 package code.schedule;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import java.time.LocalDate;
 import java.time.LocalTime;
+
+import code.ui.DrawableCalendarNote;
+import code.ui.fonts.FontManager;
 
 /**
  * This represents a blocked out portion of the user's schedule.
@@ -20,12 +30,14 @@ import java.time.LocalTime;
  * Implementation assigned to Jayden
  */
 
-public class ScheduledEvent implements Comparable<ScheduledEvent> {
+public class ScheduledEvent implements Comparable<ScheduledEvent>, DrawableCalendarNote {
     private String name;
     private LocalDate date;
     private LocalTime startTime;
     private LocalTime endTime;
     private String location = "none2";
+
+    private BufferedImage image;
 
     public ScheduledReminder hourBefore;
     public ScheduledReminder dayBefore;
@@ -123,5 +135,39 @@ public class ScheduledEvent implements Comparable<ScheduledEvent> {
             return dateComparison;
         }
         return startTime.compareTo(e.startTime);
+    }
+
+    private static final Font EVENT_FONT = FontManager.getInstance().getSmallFont();
+
+    private static final Color BG_COLOR_EVENT_TODAY = new Color(173, 217, 22);
+    private static final Color FG_COLOR_EVENT_TODAY = Color.BLACK;
+    private static final Color BG_COLOR_EVENT = new Color(209, 71, 82);
+    private static final Color FG_COLOR_EVENT = Color.WHITE;
+    private static final Color BG_COLOR_EVENT_OTHER_MONTH = new Color(237, 140, 148);
+    private static final Color FG_COLOR_EVENT_OTHER_MONTH = Color.WHITE;
+
+    @Override
+    public BufferedImage getNote(Graphics canvas, boolean today, boolean thisMonth) {
+        if (image != null) {
+            return image;
+        }
+        final int padding = 3;
+        FontMetrics fm = canvas.getFontMetrics(EVENT_FONT);
+        Rectangle2D nameBox = fm.getStringBounds(toBriefString(), canvas);
+        int nameWidth = (int)nameBox.getWidth();
+
+        image = new BufferedImage(
+                nameWidth + 2 * padding, (int)nameBox.getHeight() + padding * 2, BufferedImage.TYPE_INT_ARGB);
+        Graphics g = image.getGraphics();
+        FontManager.enableFontAntiAliasing(g);
+        g.setFont(EVENT_FONT);
+        if (today) {
+            FontManager.drawStringInRectangle(g, fm, BG_COLOR_EVENT_TODAY, FG_COLOR_EVENT_TODAY, toBriefString(), 0, 0, padding, 3);
+        } else if (thisMonth) {
+            FontManager.drawStringInRectangle(g, fm, BG_COLOR_EVENT, FG_COLOR_EVENT, toBriefString(), 0, 0, padding, 3);
+        } else {
+            FontManager.drawStringInRectangle(g, fm, BG_COLOR_EVENT_OTHER_MONTH, FG_COLOR_EVENT_OTHER_MONTH, toBriefString(), 0, 0, padding, 3);
+        }
+        return image;
     }
 }
--- code/ui/CalendarDayWidget.java
+++ code/ui/CalendarDayWidget.java
@@ -8,6 +8,7 @@ import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -122,7 +123,7 @@ public class CalendarDayWidget extends JComponent implements MouseListener {
         g.setFont(EVENT_FONT);
         if (events != null) {
             for (int i = 0, size = events.size(); i < size; i++) {
-                drawEvent(g, events.get(i).toBriefString(), i);
+                drawEvent(g, events.get(i));
             }
             // drawEvent(g, "8a Pretend this is an event", 0);
             // drawEvent(g, "12p Another event", 1);
@@ -137,12 +138,7 @@ public class CalendarDayWidget extends JComponent implements MouseListener {
         }
     }
 
-    private static final Color BG_COLOR_EVENT_TODAY = new Color(173, 217, 22);
-    private static final Color FG_COLOR_EVENT_TODAY = Color.BLACK;
-    private static final Color BG_COLOR_EVENT = new Color(209, 71, 82);
-    private static final Color FG_COLOR_EVENT = Color.WHITE;
-    private static final Color BG_COLOR_EVENT_OTHER_MONTH = new Color(237, 140, 148);
-    private static final Color FG_COLOR_EVENT_OTHER_MONTH = Color.WHITE;
+    
     private static final Font EVENT_FONT = FontManager.getInstance().getSmallFont();
     private FontMetrics eventFontMetrics;
 
@@ -154,7 +150,7 @@ public class CalendarDayWidget extends JComponent implements MouseListener {
         eventY = eventYStart;
     }
 
-    private void drawEvent(Graphics g, String brief, int eventNumber) {
+    private void drawEvent(Graphics g, DrawableCalendarNote n) {
         // int width = eventFontMetrics.stringWidth(brief) + 8;
 
         // g.setColor(BG_COLOR_EVENT);
@@ -162,13 +158,17 @@ public class CalendarDayWidget extends JComponent implements MouseListener {
         
         // g.setColor(FG_COLOR_EVENT);
         // g.drawString(brief, 7, 37 + eventNumber * 18);
-        if (today) {
+        /*if (today) {
             eventY += eventGap + FontManager.drawStringInRectangle(g, eventFontMetrics, BG_COLOR_EVENT_TODAY, FG_COLOR_EVENT_TODAY, brief, 3, eventY, 3, 3);
         } else if (thisMonth) {
             eventY += eventGap + FontManager.drawStringInRectangle(g, eventFontMetrics, BG_COLOR_EVENT, FG_COLOR_EVENT, brief, 3, eventY, 3, 3);
         } else {
             eventY += eventGap + FontManager.drawStringInRectangle(g, eventFontMetrics, BG_COLOR_EVENT_OTHER_MONTH, FG_COLOR_EVENT_OTHER_MONTH, brief, 3, eventY, 3, 3);
-        }
+        }*/
+        BufferedImage image = n.getNote(g, today, thisMonth);
+        g.drawImage(image, 3, eventY, null);
+        eventY += image.getHeight() + eventGap;
+
     }
 
     private void addEventChosen(ActionEvent e) {
