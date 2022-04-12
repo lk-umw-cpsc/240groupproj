03a187ec3801645ce929efc7a65fe0c5f8757942
Lauren Knight <lknight2@mail.umw.edu>
2022-04-12 13:10:59 -0400

    now displays message when no reminders set

--- code/ui/ReminderManagerFrame.java
+++ code/ui/ReminderManagerFrame.java
@@ -1,32 +1,36 @@
 package code.ui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.util.List;
 import java.util.concurrent.locks.Lock;
 
 import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
 
 import code.BackgroundDaemon;
 import code.schedule.ScheduledReminder;
+import code.ui.fonts.FontManager;
 
 public class ReminderManagerFrame extends JFrame {
 
     private BackgroundDaemon daemon;
 
-    private JPanel reminderContainer;
+    private Box reminderContainer;
 
     public ReminderManagerFrame(BackgroundDaemon daemon) {
         super("Your Reminders");
         this.daemon = daemon;
 
         // reminderContainer = Box.createVerticalBox();
-        reminderContainer = new JPanel();
-        reminderContainer.setLayout(new BoxLayout(reminderContainer, BoxLayout.Y_AXIS));
+        // reminderContainer = new JPanel();
+        reminderContainer = Box.createVerticalBox();
+        // reminderContainer.setLayout(new BoxLayout(reminderContainer, BoxLayout.Y_AXIS));
         // reminderContainer.setPreferredSize(new Dimension(600, 400));
 
         reminderContainer.add(Box.createVerticalGlue());
@@ -36,12 +40,12 @@ public class ReminderManagerFrame extends JFrame {
         JScrollPane scrollPane = new JScrollPane(reminderContainer,
                 JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
                 JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        
+
         scrollPane.setPreferredSize(new Dimension(600, 400));
         scrollPane.getVerticalScrollBar().setUnitIncrement(UIConstants.JSCROLLPANE_VERTICAL_SCROLLBAR_INCREMENT);
         
         scrollPane.setBorder(null);
-        add(scrollPane);
+        add(scrollPane, BorderLayout.CENTER);
         pack();
         setLocationRelativeTo(null);
     }
@@ -61,7 +65,22 @@ public class ReminderManagerFrame extends JFrame {
         l.lock();
         List<ScheduledReminder> reminders = daemon.getReminders();
         if (reminders.isEmpty()) {
-            
+            Box emptyRemindersBox = Box.createHorizontalBox();
+            emptyRemindersBox.setBorder(
+                    new EmptyBorder(UIConstants.PADDING, UIConstants.PADDING, 
+                    UIConstants.PADDING, UIConstants.PADDING));
+
+            emptyRemindersBox.add(Box.createHorizontalGlue());
+            JLabel label;
+            emptyRemindersBox.add(label = new JLabel("You don't currently have any set reminders!"));
+            label.setFont(FontManager.getInstance().getRegularFont());
+            emptyRemindersBox.add(Box.createHorizontalGlue());
+
+            reminderContainer.add(Box.createVerticalGlue());
+            reminderContainer.add(emptyRemindersBox);
+            reminderContainer.add(Box.createVerticalGlue());
+            reminderContainer.setMaximumSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
+            emptyRemindersBox.setMaximumSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
         } else {
             for (ScheduledReminder r : reminders) {
                 reminderContainer.add(new ReminderUIEntry(this, r).getContainer());
@@ -70,6 +89,7 @@ public class ReminderManagerFrame extends JFrame {
         reminderContainer.add(Box.createVerticalGlue());
 
         l.unlock();
+        pack();
         reminderContainer.revalidate();
         reminderContainer.repaint();
     }
